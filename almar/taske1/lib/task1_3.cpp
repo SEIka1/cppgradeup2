c5fb946c452d61674438f1111a466b919cfd60c2
Marfina <almarfina@gmail.com>
2026-07-06 22:35:09 +0300

    [TASK-1_3] end

--- almar/taske1/lib/task1_3.cpp
+++ almar/taske1/lib/task1_3.cpp
@@ -16,15 +16,14 @@
 #include <iostream>
 #include <vector>
 #include <string>
-std::vector<int> finding_password(unsigned int begin, unsigned int end, unsigned int real_key)
+std::vector<unsigned int> finding_password(unsigned int begin, unsigned int end, unsigned int real_key)
 {
-    std::vector<int> script_massive;
+    std::vector<unsigned int> script_massive;
     for ( unsigned int script_key = end; script_key >= begin && script_key <= end; script_key--)
     {
         if(script_key == real_key)
         {
             script_massive.push_back(script_key);
-            std::cout << "Gallery is unlocked. Enjoy";
             break;
         }
     }
@@ -39,55 +38,57 @@ int main()
     unsigned int const real_key = 999888;
     std::cout << "Input the begining of range (6 digits): ";
     std::cin >> begin;
-    for (char digits : begin)
-        {
-            if (!std::isdigit(digits))
-            {
-                std::cout << "Error: string contains non-numeric characters." << std::endl;
-                return 1;
-            }
-        }
     if (begin.length() != 6)
     {
-        std::cout << "Error: you didn't write 6 digits." << std::endl;
+        std::cout << "Error: you didn't write 6 digits.\n";
         return 1;
+    }
+    for (char digits : begin)
+    {
+        if (!std::isdigit(digits))
+        {
+            std::cout << "Error: string contains non-numeric characters.\n";
+            return 1;
+        }
     }
 
     std::cout << "Input the end of range (6 digits): ";
     std::cin >> end;
-    for (char digits : end)
-        {
-            if (!std::isdigit(digits))
-            {
-                std::cout << "Error: string contains non-numeric characters." << std::endl;
-                return 1;
-            }
-        }
-    if (end.length() != 6)
+     if (end.length() != 6)
     {
-        std::cout << "Error: you didn't write 6 digits." << std::endl;
+        std::cout << "Error: you didn't write 6 digits.\n";
         return 1;
     }
+    for (char digits : end)
+    {
+        if (!std::isdigit(digits))
+        {
+            std::cout << "Error: string contains non-numeric characters.\n";
+            return 1;
+        }
+    }
+
 
     unsigned int begin_int = static_cast<unsigned int>(std::stoul(begin));
     unsigned int end_int = static_cast<unsigned int>(std::stoul(end));
      if(end_int < begin_int)
     {
-        std::cout << "Error: begining must be less than end of out range." << std::endl;
+        std::cout << "Error: begining must be less than end of out range or equal to it.\n";
         return 1;
     }
 
-    std::vector<int> result_script_massive = finding_password(begin_int, end_int, real_key);
+    std::vector<unsigned int> result_script_massive = finding_password(begin_int, end_int, real_key);
     if (result_script_massive.empty())
     {
-        std::cout << "Password haven't been found." << "\n";
+        std::cout << "Password haven't been found.\n";
     }
     else
     {
-        std::cout << "Check the accuracy of our function: " << '\n';
+        std::cout << "Check the accuracy of our function:\n";
          for (int our_key : result_script_massive)
         {
             std::cout << "Our script key: " << our_key << " = " << real_key << " real key" << std::endl;
+            std::cout << "Gallery is unlocked. Enjoy";
         }
     }
     return 0;
